2caf615b1ee65ee84868d358e8da8b3be447bc2a
Przemysław Jacewicz <przemyslaw.jacewicz@gmail.com>
2021-05-27 09:11:36 +0200

    updating...

--- src/main/scala/pl/epsilondeltalimit/analyzer/StackExchangeDataDumpAnalyzerSingle.scala
+++ src/main/scala/pl/epsilondeltalimit/analyzer/StackExchangeDataDumpAnalyzerSingle.scala
@@ -19,7 +19,7 @@ object StackExchangeDataDumpAnalyzerSingle {
   private[this] val logger = Logger.getLogger(StackExchangeDataDumpAnalyzerSingle.getClass.getSimpleName)
 
   def main(args: Array[String]): Unit = {
-    throw new NullPointerException
+    throw new NullPointerException()
 
     val startDate = args(0)
     val endDate = args(1)
