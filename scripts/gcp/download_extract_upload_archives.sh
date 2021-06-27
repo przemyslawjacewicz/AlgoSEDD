12e21920d0fa040bbc11c299f0747539265adbe5
przemek <przemyslaw.jacewicz@gmail.com>
2021-06-28 00:32:53 +0200

    updating...

--- scripts/gcp/download_extract_upload_archives.sh
+++ scripts/gcp/download_extract_upload_archives.sh
@@ -1,50 +1,48 @@
 #!/bin/bash
-# downloads 7z archives from storage folder, extracts then and uploads extracted content to source folder
+# downloads 7z archives from storage folder, extracts them and uploads extracted content to source folder
 # dependencies: 7z
 
-# $1 - archive folder uri e.g. gs://stack-exchange-data-dump/2020-12-08/
+# $1 - archive folder uri containing 7z archives e.g. gs://stack-exchange-data-dump/2020-12-08/
 
 set -x #debug mode - will print commands
 
 SOURCE_FOLDER_URI=${1%/}
 
-# downloads a 7z archive from storage, extracts it and uploads extracted content to source folder
-download_extract_upload_single() {
+# downloads a 7z archive from storage folder, extracts it and uploads extracted content to source folder
+download_extract_upload_archive() {
   # $1 - archive file uri e.g. gs://stack-exchange-data-dump/2020-12-08/3dprinting.meta.stackexchange.com.7z
 
-  SOURCE_FILE_URI=${1%/}
+  ARCHIVE_FILE_URI=${1%/}
 
-  FILENAME=${SOURCE_FILE_URI##*/}
-  COMMUNITY_NAME=${FILENAME%.*}
-
-  DEST_FOLDER_URI=${SOURCE_FILE_URI%/*}/"$COMMUNITY_NAME"
+  FILENAME=${ARCHIVE_FILE_URI##*/}
+  DEST_FOLDER_URI=${ARCHIVE_FILE_URI%.*}
 
   # test if already extracted
   gsutil -q stat "$DEST_FOLDER_URI"/* &>/dev/null
   DEST_FOLDER_EXISTS=$?
 
   if [ $DEST_FOLDER_EXISTS -eq 0 ]; then
-    echo "File already processed: $SOURCE_SOURCE_FILE_URI"
+    echo "File already processed: $ARCHIVE_FILE_URI"
     echo "Exiting."
     exit 0
   fi
 
+  WORKING_DIR=$(mktemp -d)
+
   # download
-  gsutil cp $SOURCE_SOURCE_FILE_URI $WORKING_DIR
+  gsutil cp "$ARCHIVE_FILE_URI" "$WORKING_DIR"
 
   # extract
-  7z e $WORKING_DIR/$FILENAME -o $WORKING_DIR/$COMMUNITY_NAME
+  7z e "$WORKING_DIR"/"$FILENAME" -o "$WORKING_DIR"
 
   # upload
-  gsutil -m cp -r $WORKING_DIR/$COMMUNITY_NAME/* $DEST_FOLDER_URI
+  gsutil -m cp -r "$WORKING_DIR"/* "$DEST_FOLDER_URI"
+
+  # cleanup
+  rm -rf "$WORKING_DIR"
 }
 
-WORKING_DIR=$(mktemp -d)
-
 #todo: check if this glob is necessary
-for f in $(gsutil ls $SOURCE_URI/*.???.7z); do
+for f in $(gsutil ls "$SOURCE_FOLDER_URI"/*.???.7z); do
   download_extract_upload_single $f
 done
-
-# cleanup
-rm -rf $WORKING_DIR
